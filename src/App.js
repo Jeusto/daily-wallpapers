0d2330e96fdef54dddaf82d72b00f864eb47cb91
Arhun Saday <arhun.saday@etu.unistra.fr>
2021-06-23 02:14:08 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,82 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { React, useState, useEffect } from 'react';
+import { ThemeSwitch } from './ThemeSwitch';
+import { theme, ChakraProvider, Flex } from '@chakra-ui/react';
+import Wallpaper from './components/Wallpaper';
 
-function App() {
+export default function App() {
+  const [wallpapers, setWallpapers] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    'https://www.reddit.com/r/wallpaper/top/.json?t=day'
+  );
+
+  // When dom loads
+  useEffect(() => {
+    const getWallpapers = async () => {
+      const wallpapersFromApi = await fetchWallpapers();
+
+      wallpapersFromApi.data.children.forEach((el, i) => {
+        let currentWallpaper = {
+          id: el.data.id,
+          title: el.data.title,
+          score: el.data.score,
+          image: el.data.url,
+          width: `temp`,
+          height: `temp2`,
+        };
+        setWallpapers(currentList => [...currentList, currentWallpaper]);
+      });
+    };
+    getWallpapers();
+  }, []);
+
+  // Fetch wallpapers from api
+  const fetchWallpapers = async () => {
+    const res = await fetch(loadMore);
+    const data = await res.json();
+
+    return data;
+  };
+
+  const [displayButtons, setDisplayButtons] = useState('block');
+  const showButtons = e => {
+    e.preventDefault();
+    setDisplayButtons('block');
+  };
+
+  const hideButtons = e => {
+    e.preventDefault();
+    setDisplayButtons('none');
+  };
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <ThemeSwitch
+        position="absolute"
+        right="5"
+        top="5"
+        borderRadius="0.5rem"
+      />
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        flexWrap="wrap"
+        pt="3rem"
+      >
+        {' '}
+        {wallpapers.map(el => (
+          <Wallpaper
+            onMouseEnter={e => showButtons(e)}
+            onMouseLeave={e => hideButtons(e)}
+            display={displayButtons}
+            key={el.id}
+            title={el.title}
+            score={el.score}
+            image={el.image}
+            width={el.width}
+            height={el.height}
+          ></Wallpaper>
+        ))}{' '}
+      </Flex>
     </ChakraProvider>
   );
 }
-
-export default App;
--- src/ThemeSwitch.js
+++ src/ThemeSwitch.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { FiMoon, FiSun } from 'react-icons/fi';
 
-export const ColorModeSwitcher = props => {
+export const ThemeSwitch = props => {
   const { toggleColorMode } = useColorMode();
   const text = useColorModeValue('dark', 'light');
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+  const SwitchIcon = useColorModeValue(FiMoon, FiSun);
 
   return (
     <IconButton
       size="md"
-      fontSize="lg"
+      fontSize="2xl"
       aria-label={`Switch to ${text} mode`}
       variant="ghost"
       color="current"
